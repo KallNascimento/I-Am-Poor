f71d74b12c30d3470ab18547e391c64d1c1708c7
KallNascimento <iksn7@icloud.com>
2020-06-29 15:34:33 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kauê Nascimento on 29/06/20.
+//  Copyright © 2020 Kauê Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
